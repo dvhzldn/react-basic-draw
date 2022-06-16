42079831533f01977dddf598577db5026303c170
Dave Hazeldean <103963321+dvhzldn@users.noreply.github.com>
2022-06-16 23:20:30 +0100

    Tutorial completed.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Canvas from "./Components/Canvas";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="Heading">DRAW</h1>
+      <div className="Canvas">
+        <Canvas width={400} height={400} />
+      </div>
     </div>
   );
 }
-
 export default App;
